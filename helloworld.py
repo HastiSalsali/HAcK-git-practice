43ef4a2fe66e632e2f8ea7ae5c15dc28f80f6a0a
Hasti <hastisalsali@gmail.com>
2025-06-27 16:33:57 -0700

    wrote median fucntion

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print ("Hello, World!")
+def median(input):
+  input = sorted(input)
+  medianVal = 0
+  midPoint = math.floor(len(input) / 2)
+  if len(input) % 2 == 0:
+    a = input[midPoint - 1 ]
+    b = input[midPoint]
+    medianVal = (a + b) / 2
+  else:
+    medianVal = input[midPoint]
+  return medianVal
